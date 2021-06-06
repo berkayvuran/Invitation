cb82c1604e93da9f71fc00e382793fd689acf3a6
Berkay Vuran <berkaypsy@gmail.com>
2021-06-06 18:54:24 +0300

    update

---
+++ selectInvitation.sql
@@ -0,0 +1,28 @@
+select 
+g.Name + ' ' + g.Surname as Davetli,
+c.Name as Ýl,
+case g.IsBoarding
+	when 'True' then 'Evet'
+	else 'Hayýr'
+END as 'Yatýlý Mý',
+ds.Name as 'Kimin Davetlisi',
+dm.Name as 'Ulaþtýrma Yöntemi',
+case g.IsReady
+	when 'True' then 'Evet'
+	else 'Hayýr'
+END as 'Hazýr Mý',
+case g.IsGiven
+	when 'True' then 'Evet'
+	else 'Hayýr'
+END as 'Verildi Mi',
+case g.IsInvitedToTheParty
+	when 'True' then 'Evet'
+	else 'Hayýr'
+END as 'Akþama Davetli Mi'
+from Guests g
+join ContactInfos ci on g.ContactInfoId = ci.Id
+join Cities c on c.Id = ci.CityId
+join DataSources ds on g.DataSourceId = ds.Id
+join DeliveryMethods dm on g.DeliveryMethodId = dm.Id
+order by ds.Name, dm.Name, g.Name
+
