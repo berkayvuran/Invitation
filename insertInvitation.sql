103209493a9fc6de353a1e611c454cd66fbcc16c
Berkay Vuran <berkaypsy@gmail.com>
2021-06-06 19:06:05 +0300

    Create insertInvitation.sql

---
+++ insertInvitation.sql
@@ -0,0 +1,8 @@
+INSERT INTO Guests (Id, Name, Surname, ContactInfoId, DataSourceId, DeliveryMethodId, IsReady, IsGiven, IsBoarding, IsInvitedToTheParty)
+
+VALUES
+
+(169, 'Ýsim','Soyisim', 6,3,1,0,0,0,0)
+
+--Datasources 1: Berkay, 2: Nazlý, 3: Iþýl, 4: Osman, 5: Erþan
+--ContactInfo 6: Ankara, 9: Burdur, 10: Çorum, 11: Kastamonu
