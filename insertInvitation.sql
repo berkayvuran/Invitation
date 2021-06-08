3d58be29d65f9bbb9e9c981f42b5ba49c74faf80
Berkay Vuran <berkaypsy@gmail.com>
2021-06-08 21:14:04 +0300

    added

--- insertInvitation.sql
+++ insertInvitation.sql
@@ -1,8 +1,9 @@
-INSERT INTO Guests (Id, Name, Surname, ContactInfoId, DataSourceId, DeliveryMethodId, IsReady, IsGiven, IsBoarding, IsInvitedToTheParty)
+INSERT INTO Guests (Name, Surname, ContactInfoId, DataSourceId, DeliveryMethodId, IsReady, IsGiven, IsBoarding, IsInvitedToTheParty)
 
 VALUES
 
-(229, 'Mürüvvet','CAMBAZ', 16,3,1,0,0,0,0)
+('Sadullah','Bilinmiyor',17,3,1,0,0,0,0)
+
 
 --Datasources 1: Berkay, 2: Nazlý, 3: Iþýl, 4: Osman, 5: Erþan
---ContactInfo 6: Ankara, 9: Burdur, 10: Çorum, 11: Kastamonu
+--ContactInfo 6: Ankara, 9: Burdur, 10: Çorum, 11: Kastamonu, 17: Iþýl Diðer
--- selectInvitation.sql
+++ selectInvitation.sql
@@ -19,11 +19,30 @@ END as 'Verildi Mi',
 case g.IsInvitedToTheParty
 	when 'True' then 'Evet'
 	else 'Hayýr'
-END as 'Akþama Davetli Mi'
+END as 'Akþama Davetli Mi',
+g.GiftQuantity + ' Adet ' + gi.Name + ' ' + gd.Name  as 'Hediye'
 from Guests g
 join ContactInfos ci on g.ContactInfoId = ci.Id
 join Cities c on c.Id = ci.CityId
 join DataSources ds on g.DataSourceId = ds.Id
 join DeliveryMethods dm on g.DeliveryMethodId = dm.Id
+left join GiftInfos gi on g.GiftInfoId = gi.Id
+left join GiftDetails gd on gi.GiftDetailId = gd.Id
+where g.DeliveryMethodId=1
 order by ds.Name, dm.Name, g.Name
 
+select ds.Name as 'Kimin Davetlisi', count (ds.Name) as 'Davetli Sayýsý' from Guests g
+join DataSources ds on g.DataSourceId = ds.Id
+group by ds.Name
+
+select dm.Name as 'Veriliþ Yöntemi', count (dm.Name) as 'Davetli Sayýsý' from Guests g
+join DeliveryMethods dm on g.DeliveryMethodId = dm.Id
+group by dm.Name
+
+select
+CASE g.IsInvitedToTheParty
+WHEN 0 Then 'Hayýr'
+ELSE 'Evet'
+END as 'Akþama Davetli Mi',
+count(*) as 'Davetli Sayýsý' from Guests g
+group by g.IsInvitedToTheParty
